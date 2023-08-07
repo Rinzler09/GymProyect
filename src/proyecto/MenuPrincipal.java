09797f83ce90048ab045ea34a9508f5e1b06ced2
Usuario <Usuario@DESKTOP-I7DAP93>
2023-08-06 21:00:06 -0600

    Edit Menu Principal

--- src/proyecto/MenuPrincipal.java
+++ src/proyecto/MenuPrincipal.java
@@ -116,12 +116,22 @@ public class MenuPrincipal extends javax.swing.JFrame {
         menuHorarios.setMnemonic('h');
         menuHorarios.setText("HORARIOS");
         menuHorarios.setFont(new java.awt.Font("Perpetua Titling MT", 1, 12)); // NOI18N
+        menuHorarios.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                menuHorariosMouseClicked(evt);
+            }
+        });
         menuBar.add(menuHorarios);
 
         menuMensajeria.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/mensaje.png"))); // NOI18N
         menuMensajeria.setMnemonic('h');
         menuMensajeria.setText("MENSAJERIA");
         menuMensajeria.setFont(new java.awt.Font("Perpetua Titling MT", 1, 12)); // NOI18N
+        menuMensajeria.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                menuMensajeriaMouseClicked(evt);
+            }
+        });
         menuBar.add(menuMensajeria);
 
         menuFacturacion.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/facturacion.png"))); // NOI18N
@@ -175,7 +185,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
     }//GEN-LAST:event_menuMembresiasMouseClicked
 
     private void menuClientesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuClientesActionPerformed
-        
+
     }//GEN-LAST:event_menuClientesActionPerformed
 
     private void menuClientesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_menuClientesMouseClicked
@@ -196,6 +206,18 @@ public class MenuPrincipal extends javax.swing.JFrame {
         contenedorMenu.add(jframe);
         jframe.show();
     }//GEN-LAST:event_menuFacturacionMouseClicked
+
+    private void menuHorariosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_menuHorariosMouseClicked
+        Horarios jframe = new Horarios();
+        contenedorMenu.add(jframe);
+        jframe.show();
+    }//GEN-LAST:event_menuHorariosMouseClicked
+
+    private void menuMensajeriaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_menuMensajeriaMouseClicked
+        Mensajeria jframe = new Mensajeria();
+        contenedorMenu.add(jframe);
+        jframe.show();
+    }//GEN-LAST:event_menuMensajeriaMouseClicked
 
     /**
      * @param args the command line arguments
